538015560e61ee4760b8360dd1fbf6a8f93d0bd0
Clara <Clara@mbi-park-l06362.private.wireless.med.harvard.edu>
2020-01-30 10:53:30 -0500

    Added minimum mapping quality and minimum read length flag options

--- scripts/run-checker.sh
+++ scripts/run-checker.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 BAM_FILE="$1 $2"
 MOTIF="$3 $4"
+QUAL="$5 $6"
+MIN_PATH="$7 $8"
 
-perl $(which 4DN_REcount.pl) ${BAM_FILE} ${MOTIF} 
-
+perl $(which 4DN_REcount.pl) ${BAM_FILE} ${MOTIF} ${QUAL} ${MIN_PATH}
